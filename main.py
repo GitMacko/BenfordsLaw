ec456081fb69fbf3d595af71a137d1e023e742b3
GitMacko <GitMacko@example.com>
2024-06-08 16:30:11 +0200

    Random text generator and word counting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import lorem
 from collections import Counter
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,11 +11,12 @@ def clean_text(text):
     text = text.lower()
     return text
 
+
 def count_word_frequencies(text):
     clean_text_data = clean_text(text)
     words = clean_text_data.split()
     word_counts = Counter(words)
-    return word_counts
+    return word_counts, len(words), len(word_counts)
 
 def plot_pareto_distribution(word_counts):
     frequencies = np.array(list(word_counts.values()))
@@ -25,7 +27,7 @@ def plot_pareto_distribution(word_counts):
 
     plt.figure(figsize=(10, 6))
     plt.plot(cumulative_frequencies, marker='o')
-    plt.title('Rozkład Pareto - Częstości występowania słów')
+    plt.title('Prawo Benforda - Częstości występowania słów')
     plt.xlabel('Ranga słowa')
     plt.ylabel('Skumulowany udział')
     plt.grid(True)
@@ -44,12 +46,19 @@ def open_file():
 
 def analyze_text():
     text = text_box.get(1.0, END)
-    word_counts = count_word_frequencies(text)
+    word_counts, total_words, unique_words = count_word_frequencies(text)
+    total_words_label.config(text=f"Liczba wszystkich słów: {total_words}")
+    unique_words_label.config(text=f"Liczba różnych słów: {unique_words}")
     plot_pareto_distribution(word_counts)
+
+def generate_random_text():
+    random_text = lorem.text()  # Generuje losowy tekst
+    text_box.delete(1.0, END)
+    text_box.insert(END, random_text)
 
 # Tworzenie głównego okna aplikacji
 root = tk.Tk()
-root.title("Rozkład Pareto - Analiza Tekstu")
+root.title("Rozkład Benforda - Analiza Tekstu")
 
 # Tworzenie ramki tekstowej z paskiem przewijania
 frame = tk.Frame(root, bd=2, relief=tk.SUNKEN)
@@ -66,10 +75,20 @@ scrollbar.config(command=text_box.yview)
 open_button = tk.Button(root, text="Otwórz plik", command=open_file)
 open_button.grid(row=1, column=0, pady=10)
 
+# Przycisk do generowania losowego tekstu
+random_text_button = tk.Button(root, text="Generuj losowy tekst", command=generate_random_text)
+random_text_button.grid(row=2, column=0, pady=10)
+
 # Przycisk do analizy tekstu
 analyze_button = tk.Button(root, text="Analizuj tekst", command=analyze_text)
-analyze_button.grid(row=2, column=0, pady=10)
-analyze_button.grid(row=2, column=0, pady=10)
+analyze_button.grid(row=3, column=0, pady=10)
+
+# Etykiety do wyświetlania liczby słów
+total_words_label = tk.Label(root, text="Liczba wszystkich słów: 0")
+total_words_label.grid(row=1, column=0, pady=10, padx=10, sticky="w")
+
+unique_words_label = tk.Label(root, text="Liczba różnych słów: 0")
+unique_words_label.grid(row=2, column=0, pady=10, padx=10, sticky="w")
 
 # Uruchomienie głównej pętli aplikacji
 root.mainloop()
